a35a93244be37846b61c842635131bc6d913c22a
ashford <ashford.fernandes1111998@gmail.com>
2024-06-11 12:59:52 +0100

    initial

---
+++ src/pages/register.js
@@ -0,0 +1,58 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidateEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+    
+    if (usernameValue === '') {
+        setError(username, 'please enter your username');
+    } else {
+        setSuccess(username);
+
+    }
+
+   
+
+    if (passwordValue === '') {
+        setError(password, 'please enter your password');
+    }else if (passwordValue.length < 10) {
+        setError(password, 'password must be atlist 8 characters.')
+    } else {
+        setSuccess(password);
+    }
+
+ 
+}
